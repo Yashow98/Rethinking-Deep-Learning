e0571dd018ecb3f224927f18e06d27aac3b3259a
Yashow98 <yashowhoo@outlook.com>
2023-05-14 09:42:43 +0800

    Add thop

---
+++ 40_thop.py
@@ -0,0 +1,14 @@
+# @Time    : 5/14/2023 9:34 AM
+# @Author  : Yashowhoo
+# @File    : 40_thop.py
+# @Description :
+import torch
+from torchvision import models
+from thop import profile, clever_format
+
+model = models.resnet50()
+input = torch.randn(1, 3, 224, 224)
+macs, params = profile(model, inputs=(input, ))
+macs, params = clever_format([macs, params])
+print(macs, params)
+
